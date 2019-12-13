6c896876d29ef0bc1322eec09b5076827ff607b6
givka <givka.dev@gmail.com>
2019-12-13 18:58:47 +0100

    add day5 part2

--- day5/main.py
+++ day5/main.py
@@ -41,25 +41,53 @@ def update_list_new(numbers, input_number, cursor):
     return input_number
 
 
+def update_list_new_part2(numbers, cursor):
+    current_str = str(numbers[cursor])
+    current = int(current_str[-1])
+
+    immediate1 = len(current_str) > 2 and current_str[-3] == "1"
+    immediate2 = len(current_str) > 3 and current_str[-4] == "1"
+    parameter1 = numbers[cursor + 1]
+    parameter2 = numbers[cursor + 2]
+    parameter3 = numbers[cursor + 3]
+
+    parameter1 = parameter1 if immediate1 else numbers[parameter1]
+    parameter2 = parameter2 if immediate2 else numbers[parameter2]
+
+    if current == 5:
+        cursor = parameter2 if parameter1 != 0 else cursor + 3
+    elif current == 6:
+        cursor = parameter2 if parameter1 == 0 else cursor + 3
+    elif current == 7:
+        numbers[parameter3] = 1 if parameter1 < parameter2 else 0
+        cursor += 4
+    elif current == 8:
+        numbers[parameter3] = 1 if parameter1 == parameter2 else 0
+        cursor += 4
+    return cursor
+
+
 def update_list(numbers: list, input_value: int, cursor=0):
     if numbers[cursor] == 99:
         return input_value
     current_str = str(numbers[cursor])
     current = int(current_str[-1])
 
-    if current == 1 or current == 2:
+    if current in [1, 2]:
         update_list_old(numbers, cursor)
         cursor += 4
-    elif current == 3 or current == 4:
+    elif current in [3, 4]:
         input_value = update_list_new(numbers, input_value, cursor)
         cursor += 2
+    elif current in [5, 6, 7, 8]:
+        cursor = update_list_new_part2(numbers, cursor)
 
     return update_list(numbers, input_value, cursor)
 
 
 def main():
     numbers = parse()
-    print("output:", update_list(numbers, 1))
+    print(update_list(numbers, 5))
 
 
 if __name__ == "__main__":
