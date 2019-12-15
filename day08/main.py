0067e29aed0bb136f1afaa8ad8f35328f932c73b
givka <givka.dev@gmail.com>
2019-12-15 15:31:26 +0100

    add day8 part2

--- day08/main.py
+++ day08/main.py
@@ -16,16 +16,29 @@ def get_layers(data: str, width: int, height: int):
     return layers
 
 
-def get_number_occ(layer: str, number: int):
+def num_occ(layer: str, number: int):
     return len([n for n in layer if int(n) == number])
 
 
 def main():
     data = parse()
-    layers = get_layers(data, 25, 6)
-    layers_and_0_occ = [(layer, get_number_occ(layer, 0)) for layer in layers]
-    layer_with_less_0 = min(layers_and_0_occ, key=lambda t: t[1])[0]
-    print("number of 1s by 2s:", get_number_occ(layer_with_less_0, 1) * get_number_occ(layer_with_less_0, 2))
+    width = 25
+    height = 6
+    layers = get_layers(data, width, height)
+    layers_and_0_occ = [(layer, num_occ(layer, 0)) for layer in layers]
+    layer_with_less_0 = min(layers_and_0_occ, key=lambda t: t[1])[0]  # min by num of 0 occ
+    print("number of 1s * 2s:", num_occ(layer_with_less_0, 1) * num_occ(layer_with_less_0, 2))
+
+    message = ""
+    for h in range(0, height):
+        for w in range(0, width):
+            for layer in layers:
+                current = layer[w + width * h]
+                if current != "2":
+                    message += "0" if current == "1" else " "
+                    break
+        message += "\n"
+    print(message)
 
 
 if __name__ == "__main__":
