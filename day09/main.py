4170a12d8fc0908d94c94222379b6a63f0d1b7bf
givka <givka.dev@gmail.com>
2019-12-15 19:10:05 +0100

    add day 9 part 2

--- day09/main.py
+++ day09/main.py
@@ -10,7 +10,7 @@ def parse():
 
 class IntCode:
     def __init__(self, numbers: list, input_number: int, relative_number: int):
-        self.numbers = numbers
+        self.numbers = numbers.copy()
         self.numbers.extend([0 for _ in range(1, 10000)])
         self.input_number = input_number
         self.relative_number = relative_number
@@ -21,37 +21,34 @@ class IntCode:
         self.process_code()
 
     def process_code(self):
-        if self.numbers[self.cursor] == 99:
-            return
+        # removed recursion because of python recursion limit
+        while self.numbers[self.cursor] != 99:
+            self.current_str = str(self.numbers[self.cursor])
+            self.current = int(self.current_str[-1])
 
-        self.current_str = str(self.numbers[self.cursor])
-        self.current = int(self.current_str[-1])
+            if self.current == 1:
+                self.numbers[self.p(3)] = self.numbers[self.p(1)] + self.numbers[self.p(2)]
+            if self.current == 2:
+                self.numbers[self.p(3)] = self.numbers[self.p(1)] * self.numbers[self.p(2)]
+            if self.current == 3:
+                self.numbers[self.p(1)] = self.input_number
+            if self.current == 4:
+                self.outputs.append(self.numbers[(self.p(1))])
+            if self.current == 5:
+                self.cursor = self.numbers[(self.p(2))] if self.numbers[(self.p(1))] != 0 else self.cursor + 3
+            if self.current == 6:
+                self.cursor = self.numbers[(self.p(2))] if self.numbers[(self.p(1))] == 0 else self.cursor + 3
+            if self.current == 7:
+                self.numbers[(self.p(3))] = 1 if self.numbers[self.p(1)] < self.numbers[self.p(2)] else 0
+            if self.current == 8:
+                self.numbers[(self.p(3))] = 1 if self.numbers[self.p(1)] == self.numbers[self.p(2)] else 0
+            if self.current == 9:
+                self.relative_number += self.numbers[self.p(1)]
 
-        if self.current == 1:
-            self.numbers[self.p(3)] = self.numbers[self.p(1)] + self.numbers[self.p(2)]
-        if self.current == 2:
-            self.numbers[self.p(3)] = self.numbers[self.p(1)] * self.numbers[self.p(2)]
-        if self.current == 3:
-            self.numbers[self.p(1)] = self.input_number
-        if self.current == 4:
-            self.outputs.append(self.numbers[(self.p(1))])
-        if self.current == 5:
-            self.cursor = self.numbers[(self.p(2))] if self.numbers[(self.p(1))] != 0 else self.cursor + 3
-        if self.current == 6:
-            self.cursor = self.numbers[(self.p(2))] if self.numbers[(self.p(1))] == 0 else self.cursor + 3
-        if self.current == 7:
-            self.numbers[(self.p(3))] = 1 if self.numbers[self.p(1)] < self.numbers[self.p(2)] else 0
-        if self.current == 8:
-            self.numbers[(self.p(3))] = 1 if self.numbers[self.p(1)] == self.numbers[self.p(2)] else 0
-        if self.current == 9:
-            self.relative_number += self.numbers[self.p(1)]
-
-        if self.current in [1, 2, 7, 8]:
-            self.cursor += 4
-        if self.current in [3, 4, 9]:
-            self.cursor += 2
-
-        self.process_code()
+            if self.current in [1, 2, 7, 8]:
+                self.cursor += 4
+            if self.current in [3, 4, 9]:
+                self.cursor += 2
 
     def p(self, index):
         mode = int(self.current_str[-(index + 2)]) if len(self.current_str) > (index + 1) else 0
@@ -65,8 +62,12 @@ class IntCode:
 
 def main():
     numbers = parse()
-    int_code = IntCode(numbers, 1, 0)
-    print("outputs:", int_code.outputs)
+
+    int_code_part1 = IntCode(numbers, 1, 0)
+    print("outputs part1:", int_code_part1.outputs)
+
+    int_code_part2 = IntCode(numbers, 2, 0)
+    print("outputs part2:", int_code_part2.outputs)
 
 
 if __name__ == "__main__":
