fad957117e1202909052a94685f5043e064c6f10
givka <givka.dev@gmail.com>
2019-12-19 12:20:57 +0100

    add command pattern in day17 for part2

--- day17-part1/main.py
+++ day17-part1/main.py
@@ -1,5 +1,5 @@
 import csv
-from collections import deque
+import re
 from dataclasses import dataclass
 
 
@@ -11,6 +11,7 @@ class IntCode:
         self.cursor = 0
         self.current = 0
         self.current_str = ""
+        self.pattern = []
 
     def is_finished(self):
         return self.numbers[self.cursor] == 99
@@ -98,6 +99,7 @@ class Robot:
         self.cursor = vec2(0, 0)
         self.pos = vec2(0, 0)
         self.intersections = []
+        self.dir = vec2(0, 0)
 
     def test(self, m):
         for i in m:
@@ -108,10 +110,18 @@ class Robot:
 
         if char == "^" or char == "v" or char == "<" or char == ">":
             self.pos = vec2(self.cursor.x, self.cursor.y)
+            if char == "^":
+                self.dir = Movement.NORTH
+            if char == "v":
+                self.dir = Movement.SOUTH
+            if char == "<":
+                self.dir = Movement.WEST
+            if char == ">":
+                self.dir = Movement.EAST
 
         if char == "\n":
             self.cursor.x = 0
-            self.cursor.y += 1
+            self.cursor.y -= 1
         else:
             self.map[vec2(self.cursor.x, self.cursor.y)] = char
             self.cursor.x += 1
@@ -127,7 +137,7 @@ class Robot:
         max_w = max(self.map, key=lambda t: t.x).x
         max_h = max(self.map, key=lambda t: t.y).y
         string = ""
-        for y in range(min_h, max_h + 1):
+        for y in sorted(range(min_h, max_h + 1), reverse=True):
             for x in range(min_w, max_w + 1):
                 if vec2(x, y) not in self.map:
                     continue
@@ -139,14 +149,11 @@ class Robot:
         print(string)
 
     def get_intersections(self):
-
         for pos in self.map:
             if self.map[pos] != "#":
                 continue
-            neighbours = [pos + Movement.NORTH,
-                          pos + Movement.SOUTH,
-                          pos + Movement.WEST,
-                          pos + Movement.EAST]
+            neighbours = [pos + Movement.NORTH, pos + Movement.SOUTH,
+                          pos + Movement.WEST, pos + Movement.EAST]
 
             intersection = True
             for neighbour in neighbours:
@@ -157,8 +164,84 @@ class Robot:
                 self.intersections.append(vec2(pos.x, pos.y))
 
     def get_sum_alignment_parameters(self):
-        alignment_parameters = [i.x * i.y for i in self.intersections]
+        alignment_parameters = [abs(i.x * i.y) for i in self.intersections]
         return sum(alignment_parameters)
+
+    def get_command_pattern(self):
+        neighbours = [Movement.NORTH, Movement.SOUTH, Movement.WEST, Movement.EAST]
+
+        commands = []
+        command = None
+        turn = ""
+
+        for n in neighbours:
+            if self.map[self.pos + n] == "#":
+                command = vec2(0, 0)
+                turn = self.new_turn(n)
+                self.dir = n
+
+        while len([key for key in self.map if self.map[key] == "#"]) != 0:
+            command += self.dir
+            self.pos += self.dir
+            self.map[self.pos] = ";"
+            pos = self.pos + self.dir
+            if pos in self.map and self.map[pos] == "#":
+                continue
+            for n in neighbours:
+                if n == self.dir or n == -self.dir:
+                    continue
+                if self.pos + n not in self.map:
+                    continue
+                if self.map[self.pos + n] == "#":
+                    commands.append((turn, abs(command.x + command.y)))
+                    command = vec2(0, 0)
+                    turn = self.new_turn(n)
+                    self.dir = n
+                    break
+
+        commands.append((turn, abs(command.x + command.y)))  # last command
+
+        unique_commands = list(set(commands))
+
+        string = ''
+        for command in commands:
+            string += str(unique_commands.index(command))
+
+        patterns = ['' for _ in range(0, 3)]  # give patterns of given length
+        assert find_pattern(string, patterns)
+
+        command_pattern = []
+        for pattern in patterns:
+            command_pattern.append([unique_commands[int(char)] for char in pattern])
+
+        return command_pattern
+
+    def new_turn(self, new_pos):
+        if self.dir == Movement.NORTH:
+            return "R" if new_pos == Movement.EAST else "L"
+        if self.dir == Movement.EAST:
+            return "R" if new_pos == Movement.SOUTH else "L"
+        if self.dir == Movement.SOUTH:
+            return "R" if new_pos == Movement.WEST else "L"
+        if self.dir == Movement.WEST:
+            return "R" if new_pos == Movement.NORTH else "L"
+
+
+def find_pattern(string: str, patterns: list, level=1):
+    for i in range(1, len(string)):
+        look_for = string[0:i]
+        patterns[level - 1] = look_for
+        z = re.finditer(look_for, string)
+        matches = [zz.start() for zz in z]
+        if level != len(patterns) and len(matches) == 1:  # we want groups of 2 minimum
+            break
+        string_to_empty = re.sub(look_for, '', string)
+        if level == len(patterns) and len(string_to_empty) == 0:
+            return True
+        if level < len(patterns):
+            if find_pattern(string_to_empty, patterns, level + 1):
+                return True
+    return False
 
 
 def main():
@@ -170,8 +253,10 @@ def main():
         int_code.process_code(robot.input_callback, robot.output_callback)
 
     robot.get_intersections()
-    robot.print_map()
+    # robot.print_map()
     print("sum alignment parameters:", robot.get_sum_alignment_parameters())
+    command_pattern = robot.get_command_pattern()
+    print(command_pattern)
 
 
 if __name__ == "__main__":
