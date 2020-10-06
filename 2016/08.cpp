59ea7c9151329b04381304e7fe9c5b931696126d
givka <givka.dev@gmail.com>
2020-10-06 19:48:02 +0200

    reformat 2016/08.cpp

--- 2016/08.cpp
+++ 2016/08.cpp
@@ -14,53 +14,12 @@ const int WIDTH = 50;
 char map[HEIGHT][WIDTH];
 
 vector<string> split(const string &str, char c) {
+    istringstream iss(str);
     vector<string> strings;
-    int index = str.find(c);
-    strings.push_back(str.substr(0, index));
-    strings.push_back(str.substr(index + 1));
+    for (string sub_str; getline(iss, sub_str, c);) {
+        strings.push_back(sub_str);
+    }
     return strings;
-}
-
-void create_rect(vector<string> &command) {
-    auto strings = split(command[1], 'x');
-    int xx = stoi(strings[0]);
-    int yy = stoi(strings[1]);
-
-    for (int y = 0; y < yy; ++y) {
-        for (int x = 0; x < xx; ++x) {
-            map[y][x] = '#';
-        }
-    }
-}
-
-void rotate_column(vector<string> &command) {
-    auto strings = split(command[2], '=');
-    int where = stoi(strings[1]);
-    int by = stoi(command[4]);
-
-    vector<char> chars;
-    for (int y = 0; y < HEIGHT; y++) {
-        chars.emplace_back(map[(y - by + HEIGHT) % HEIGHT][where]);
-    }
-
-    for (int y = 0; y < HEIGHT; y++) {
-        map[y][where] = chars[y];
-    }
-}
-
-void rotate_row(vector<string> &command) {
-    auto strings = split(command[2], '=');
-    int where = stoi(strings[1]);
-    int by = stoi(command[4]);
-
-    vector<char> chars;
-    for (int x = 0; x < WIDTH; x++) {
-        chars.emplace_back(map[where][(x - by + WIDTH) % WIDTH]);
-    }
-
-    for (int x = 0; x < WIDTH; x++) {
-        map[where][x] = chars[x];
-    }
 }
 
 int main() {
@@ -83,17 +42,47 @@ int main() {
     }
 
     for (vector<string> &command : commands) {
-        if (command[0] == "rect")
-            create_rect(command);
-        else if (command[1] == "column")
-            rotate_column(command);
-        else
-            rotate_row(command);
+        if (command[0] == "rect") {
+            auto strings = split(command[1], 'x');
+            int xx = stoi(strings[0]);
+            int yy = stoi(strings[1]);
+
+            for (int y = 0; y < yy; ++y) {
+                for (int x = 0; x < xx; ++x) {
+                    map[y][x] = '#';
+                }
+            }
+        }
+        else if (command[0] == "rotate") {
+            auto strings = split(command[2], '=');
+            int where = stoi(strings[1]);
+            int by = stoi(command[4]);
+
+            if (command[1] == "column") {
+                vector<char> chars;
+                for (int y = 0; y < HEIGHT; y++) {
+                    chars.emplace_back(map[(y - by + HEIGHT) % HEIGHT][where]);
+                }
+
+                for (int y = 0; y < HEIGHT; y++) {
+                    map[y][where] = chars[y];
+                }
+            }
+            else {
+                vector<char> chars;
+                for (int x = 0; x < WIDTH; x++) {
+                    chars.emplace_back(map[where][(x - by + WIDTH) % WIDTH]);
+                }
+
+                for (int x = 0; x < WIDTH; x++) {
+                    map[where][x] = chars[x];
+                }
+            }
+        }
     }
 
     int count = 0;
 
-    PRINTLN();
     for (int y = 0; y < HEIGHT; ++y) {
         for (int x = 0; x < WIDTH; ++x) {
             if (map[y][x] == '#') {
@@ -101,9 +90,8 @@ int main() {
             }
             PRINT(map[y][x]);
         }
-        PRINTLN();
+        PRINTLN("");
     }
-    PRINTLN();
 
     PRINTLN(count);
 }
